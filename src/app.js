a8db1f3ba10aaef3698c11d637d27ea2085115fb
e <lambert1129@foxmail.com>
2018-10-01 21:55:09 +0800

    添加icon  和 iconPositon

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Button from './g-button.vue'
 
+console.log(Button);
 Vue.component('g-button',Button);
 
 new Vue({
--- src/g-button.vue
+++ src/g-button.vue
@@ -1,11 +1,17 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <span class="content">
+            <slot ></slot>
+        </span>
+    </button>
 </template>
 
 <script>
     export default {
-        name: "button.vue"
-
+        props:['icon','iconPosition']
     }
 </script>
 
@@ -13,15 +19,28 @@
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
-        padding: 0 1em;
+        padding: 0 .5em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display:inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+
         &:hover {
             border-color: var(--border-color-hover);
             cursor: pointer;
+
         }
         &:active {background-color: var(--button-active-bg);}
         &:focus {outline: none;}
+        > .content{order:2;}
+        > .icon{order:1;margin-right:.3em;}
+
+        &.icon-right{
+            > .content{order:1;}
+            > .icon{order:2;margin-right:0;margin-left:.3em;}
+        }
     }
 </style>
