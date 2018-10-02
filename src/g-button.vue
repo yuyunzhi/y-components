3fdc2ddd8330d91696db05d6406c1eb71ad8822b
e <lambert1129@foxmail.com>
2018-10-02 10:55:38 +0800

    增加组件外部增加click事件，组件内部emit触发事件

--- src/g-button.vue
+++ src/g-button.vue
@@ -1,7 +1,9 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-        <g-icon class="loading" name="loading"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
+    @click="$emit('click')"
+    >
+        <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+        <g-icon class="icon loading" v-if="loading" name="loading"></g-icon>
         <span class="content">
             <slot ></slot>
         </span>
@@ -13,6 +15,10 @@
         //props:['icon','iconPosition']
         props:{
             icon:{},
+            loading:{
+                type:Boolean,
+                default:false,
+            },
             iconPosition:{
                 type:String,
                 default:'left',
