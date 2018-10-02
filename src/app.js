e8c17176e2a2203229921c50ab191df415abdd5a
e <lambert1129@foxmail.com>
2018-10-02 14:03:37 +0800

    完成buttonGroup样式重叠解决方案，完成g-button-group子元素检测及弹出警告

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Button from './g-button.vue'
 import Icon from './g-icon.vue'
+import ButtonGroup from './g-button-group'
 
 
 Vue.component('g-button',Button);
 Vue.component('g-icon',Icon);
+Vue.component('g-button-group',ButtonGroup);
 new Vue({
     el:'#app',
     data:{
---
+++ src/g-button-group.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted(){
+            for(let node of this.$el.children){
+                let name = node.nodeName.toLowerCase()
+                if(name !== 'button'){
+                    console.warn(`g-button-group 的子元素应该全是 g-button,但你写的是${name}`)
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .g-button-group{
+        display:inline-flex;
+        vertical-align: middle;
+        > .g-button{
+            border-radius:0;
+            margin-left:-1px;
+            &:first-child{
+                border-top-left-radius:var(--border-radius);
+                border-bottom-left-radius:var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius:var(--border-radius);
+            }
+            &:hover{
+                border-color:#666;
+                position:relative;
+                z-index: 1;
+            }
+
+        }
+    }
+</style>
