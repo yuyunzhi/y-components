1bcdb7acfb3d65a2a9f470da0f534e9aa85c9451
e <lambert1129@foxmail.com>
2018-10-02 14:45:28 +0800

    安装chai chai-spies

--- src/g-button-group.vue
+++ src/g-button-group.vue
@@ -8,7 +8,7 @@
     export default {
         mounted(){
             for(let node of this.$el.children){
-                let name = node.nodeName.toLowerCase()
+                let name = node.nodeName.toLowerCase();
                 if(name !== 'button'){
                     console.warn(`g-button-group 的子元素应该全是 g-button,但你写的是${name}`)
                 }
@@ -23,7 +23,9 @@
         vertical-align: middle;
         > .g-button{
             border-radius:0;
-            margin-left:-1px;
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
             &:first-child{
                 border-top-left-radius:var(--border-radius);
                 border-bottom-left-radius:var(--border-radius);
