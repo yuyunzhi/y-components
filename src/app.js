3abef5b06d9ff21439ddb911cd210fae077e1d92
e <lambert1129@foxmail.com>
2018-10-10 09:45:40 +0800

    grid布局及响应式功能基本完成

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 
 Vue.component('g-button',Button);
 Vue.component('g-icon',Icon);
 Vue.component('g-button-group',ButtonGroup);
-Vue.component('g-input',Input)
+Vue.component('g-input',Input);
+Vue.component('g-row', Row);
+Vue.component('g-col', Col);
+
+
 new Vue({
     el:'#app',
     data:{
