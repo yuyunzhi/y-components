6b6ee3fa6595066fe2c30dbb9bc91b4ea909e5e6
e <lambert1129@foxmail.com>
2018-11-01 14:32:57 +0800

    增加递归组件

--- src/app.js
+++ src/app.js
@@ -1,52 +1,63 @@
-import Vue from 'vue'
+
 import Button from './button'
-import Icon from './icon'
 import ButtonGroup from './button-group'
-import Input from './input'
-import Row from './row'
+import Cascader from './cascader'
 import Col from './col'
-import Layout from './layout'
-import Header from './header'
-import Sider from './sider'
-import Content from './content'
-import Footer from './footer'
-import Toast from './toast.vue'
-import plugin from './plugin.js'
-import Tabs from './tabs'
-import TabsHead from './tabs-head'
-import TabsBody from './tabs-body'
-import TabsItem from './tabs-item'
-import TabsPane from './tabs-pane'
 import Collapse from './collapse'
 import CollapseItem from './collapse-item'
+import Content from './content'
+import Footer from './footer'
+import Header from './header'
+import Icon from './icon'
+import Input from './input'
+import Layout from './layout'
+import plugin from './plugin.js'
 import Popover from './popover'
+import Row from './row'
+import Sider from './sider'
+import Tabs from './tabs'
+import TabsBody from './tabs-body'
+import TabsHead from './tabs-head'
+import TabsItem from './tabs-item'
+import TabsPane from './tabs-pane'
+import Toast from './toast.vue'
+import Vue from 'vue'
+
 
 Vue.component('y-button', Button);
-Vue.component('y-icon', Icon);
 Vue.component('y-button-group', ButtonGroup);
-Vue.component('y-input', Input);
-Vue.component('y-row', Row);
+Vue.component('y-cascader', Cascader);
 Vue.component('y-col', Col);
-Vue.component('y-layout', Layout);
-Vue.component('y-header', Header);
-Vue.component('y-content', Content);
-Vue.component('y-footer', Footer);
-Vue.component('y-sider', Sider);
-Vue.component('y-toast', Toast);
-Vue.use(plugin);
-Vue.component('y-tabs', Tabs);
-Vue.component('y-tabs-head', TabsHead);
-Vue.component('y-tabs-body', TabsBody);
-Vue.component('y-tabs-item', TabsItem);
-Vue.component('y-tabs-pane', TabsPane);
 Vue.component('y-collapse', Collapse);
 Vue.component('y-collapse-item', CollapseItem);
+Vue.component('y-content', Content);
+Vue.component('y-footer', Footer);
+Vue.component('y-header', Header);
+Vue.component('y-icon', Icon);
+Vue.component('y-input', Input);
+Vue.component('y-layout', Layout);
 Vue.component('y-popover', Popover);
+Vue.component('y-row', Row);
+Vue.component('y-sider', Sider);
+Vue.component('y-tabs', Tabs);
+Vue.component('y-tabs-body', TabsBody);
+Vue.component('y-tabs-head', TabsHead);
+Vue.component('y-tabs-item', TabsItem);
+Vue.component('y-tabs-pane', TabsPane);
+Vue.component('y-toast', Toast);
+Vue.use(plugin);
 
 new Vue({
     el:'#app',
     data:{
-
+        source:[
+            {name:'江苏', children:[{name:'A',
+                    children:[{name:'B'},{name:'B'},{name:'B'},{name:'B'}]
+                },{name:'A'},{name:'A'},{name:'A'}]},
+            {name:'北京', children:[{name:'B'},{name:'B'},{name:'B'},{name:'B'}]},
+            {name:'广州',children:[{name:'C'},{name:'C'},{name:'C'},{name:'C'}]},
+            {name:'西藏',children:[{name:'D'},{name:'D'},{name:'D'},{name:'D'}]},
+        ]
     },
     methods:{
 
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,14 +1,35 @@
 <template>
-    
+    <div class="cascader">
+        <div class="trigger">
+            <slot></slot>
+        </div>
+        <div class="popover">
+            <div v-for="item in source">
+                <cascader-x :info="item"></cascader-x>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import CascaderX from './cascader-x'
     export default {
-        name: "YCascader"
+        name: "YCascader",
+        components:{
+            'cascader-x':CascaderX
+        },
+        props:{
+          source:{
+              type:Array,
+
+          }
+        },
     }
 </script>
 
 <style scoped lang="scss">
     @import "var";
+    .cascader{
 
+    }
 </style>
