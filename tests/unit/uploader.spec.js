d6717f60e0a689e49e09b823033d06bfbccfd442
e <lambert1129@foxmail.com>
2019-01-11 15:25:02 +0800

    xxx

--- tests/unit/uploader.spec.js
+++ tests/unit/uploader.spec.js
@@ -11,6 +11,7 @@ describe('Uploader.vue', () => {
     it('存在.', () => {
         expect(Uploader).to.exist
     })
+
     it('可以上传一个文件', () => {
         let stub = sinon.stub($http, 'post').callsFake((url, options) => {
             setTimeout(function () {
@@ -23,6 +24,7 @@ describe('Uploader.vue', () => {
                 name: 'file',
                 action: '/upload',
                 method: 'post',
+                accept:'image/*',
                 parseResponse: (response) => {
                     let object = JSON.parse(response)
                     return `/preview/${object.id}`
@@ -33,6 +35,8 @@ describe('Uploader.vue', () => {
             listeners: {
                 'update:fileList': (fileList) => { wrapper.setProps({fileList}) },
                 'uploaded': () => {
+
+                    console.log(1);
                     expect(wrapper.find('use').exists()).to.eq(false)
                     expect(wrapper.props().fileList[0].url).to.eq('/preview/123123')
                     stub.restore()
@@ -41,15 +45,20 @@ describe('Uploader.vue', () => {
                 }
             }
         })
+        //触发button
         wrapper.find('#x').trigger('click')
+
+        //找到input ,创建新文件
         let inputWrapper = wrapper.find('input[type="file"]')
         let input = inputWrapper.element
         let file1 = new File(['xxxxxxxxx'], 'xxx.txt')
 
+        //给input 设置文件
         const data = new DataTransfer()
         data.items.add(file1)
         input.files = data.files;
 
+        //上传后期待出现loading
         let use = wrapper.find('use').element
         expect(use.getAttribute('xlink:href')).to.eq('#i-loading')
 
